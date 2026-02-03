4029ea7fb7732ce6f0357692b0af039950fa473a
Chinonso Chikelue <163886675+chinonsochikelue@users.noreply.github.com>
2026-02-03 23:37:42 +0100

    fix(ci): remove invalid sync command and use tharos analyze .

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import { title } from '@/lib/layout.shared'
 import { baseUrl, createMetadata } from '@/lib/metadata'
 import '@/styles/globals.css'
@@ -176,6 +177,18 @@ export default function Layout({ children }: LayoutProps<'/'>) {
       suppressHydrationWarning
     >
       <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MF8G5GT2');`,
+          }}
+        />
         {/* Additional SEO meta tags */}
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
@@ -207,6 +220,13 @@ export default function Layout({ children }: LayoutProps<'/'>) {
         />
       </head>
       <Body tree={source.pageTree}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MF8G5GT2"
+            height="0" width="0" style="display:none;visibility:hidden">
+          </iframe>
+        </noscript>
+
         <NextProvider>
           <TreeContextProvider tree={source.pageTree}>
             <Providers>{children}</Providers>
