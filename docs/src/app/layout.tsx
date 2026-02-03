41ecc8efeb915fadf20ae8f9834d77e4d87f540b
Chinonso Chikelue <163886675+chinonsochikelue@users.noreply.github.com>
2026-02-03 23:40:49 +0100

    fix(ci): remove invalid sync command and use tharos analyze .

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -223,7 +223,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MF8G5GT2"
-            height="0" width="0" style="display:none;visibility:hidden">
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}>
           </iframe>
         </noscript>
 
